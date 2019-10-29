8334f6dff8bbd3d1b5ecd01578f64c5a8bb8453a
pangtuzimm <pangtuzimm@gmail.com>
2019-10-28 22:34:31 -0700

    create a login page with email and password

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-// import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function App() {
+  function validForm() {
+    return email.length > 0 && password.length > 0;
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React Now
-        </a> */}
-
-      </header>
+      <form class="App-login">
+        <FormGroup>
+          <FormLabel>Email</FormLabel>
+          <FormControl 
+            autoFocus 
+            type="email" 
+            value={email} 
+            onChange={e => setEmail(e.target.value)} 
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            type="password" 
+            value={password} 
+            onChange={e => setPassword(e.target.value)} 
+          />
+        </FormGroup>
+        <Button disabled={!validForm()} type="submit">
+          Login
+        </Button>
+      </form>
     </div>
   );
 }
-
-export default App;
