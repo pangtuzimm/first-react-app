c0175263168aa7273a0e088ac6bee1cf6f0e3946
pangtuzimm <pangtuzimm@gmail.com>
2019-10-28 22:05:47 -0700

    add files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React Now
+        </a> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
